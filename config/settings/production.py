325385f60c04ea2c4bb33803bfd3445822438f02
koya-namba <namba.koya@arakawa-lab.com>
2021-10-06 18:12:45 +0900

    add url

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['', '127.0.0.1']
+ALLOWED_HOSTS = ['https://slack-work-out.herokuapp.com/', '127.0.0.1']
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
